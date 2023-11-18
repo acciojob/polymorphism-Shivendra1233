2b08831968d9f35695a5df037b29f81ed35fb8df
Shivendra1233 <132485033+Shivendra1233@users.noreply.github.com>
2023-11-17 19:59:37 -0800

    Main.java
    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,37 +1,27 @@
 package com.driver;
+
 public class Main {
-    // Task 1: Create a class Product inside Main class
-    static class Product {
-        // Task 3: Create a method with the given definition
-        public int product(int x, int y) {
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        p.product(10, 20);
+
+        p.product(10, 20, 30);
+
+        p.product(10.0, 20.0);
+    }
+
+    public static class Product{
+        public int product(int x, int y){
             return x * y;
         }
 
-        // Task 4: Create an overloaded method with a different parameter list
         public int product(int x, int y, int z) {
             return x * y * z;
         }
 
-        // Task 5: Create another overloaded method with a different parameter list
         public double product(double x, double y) {
             return x * y;
         }
-    }
-
-    public static void main(String[] args) {
-        // Task 2: Create an object of Product in Main function called p
-        Product p = new Product();
-
-        // Task 3: Call the method with the given definition
-        int result1 = p.product(2, 3);
-        System.out.println("Result of product(int x, int y): " + result1);
-
-        // Task 4: Call the overloaded method with a different parameter list
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Result of product(int x, int y, int z): " + result2);
-
-        // Task 5: Call the second overloaded method with a different parameter list
-        double result3 = p.product(2.5, 3.5);
-        System.out.println("Result of product(double x, double y): " + result3);
     }
 }
